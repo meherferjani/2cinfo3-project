b506205e73d8ac8d6c5561268478c2875aaf3386
SeifEddineJridi <seifeddine.jridi@esprit.tn>
2024-04-12 21:55:14 +0100

    generer des entiers aleatoire

--- src/ArraySort.java
+++ src/ArraySort.java
@@ -2,4 +2,13 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class ArraySort {
+	
+	  public static int[] generateRandomArray(int length) {
+	        int[] array = new int[length];
+	        Random random = new Random();
+	        for (int i = 0; i < length; i++) {
+	            array[i] = random.nextInt(100); // Generates integers between 0 and 99
+	        }
+	        return array;
+	    }
     }
