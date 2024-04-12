960b50bbeb65ef2232e7c6bda3ba371eeebd5e35
SeifEddineJridi <seifeddine.jridi@esprit.tn>
2024-04-12 21:55:43 +0100

    ajouter la fonction d'affichage

--- src/ArraySort.java
+++ src/ArraySort.java
@@ -11,4 +11,11 @@ public class ArraySort {
 	        }
 	        return array;
 	    }
+	  
+	    public static void printArray(int[] array) {
+	        for (int num : array) {
+	            System.out.print(num + " ");
+	        }
+	        System.out.println();
+	    }
     }
