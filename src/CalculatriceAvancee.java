a755bff42ebfa1f1f85fa910bdc9f3746ed251b5
rtlili9 <ranim.bentlili@esprit.tn>
2024-04-13 01:40:36 +0200

    Method1

---
+++ src/CalculatriceAvancee.java
@@ -0,0 +1,15 @@
+public class CalculatriceAvancee {
+    public static void main(String[] args) {
+        // Exemples d'utilisation des méthodes de calcul
+        double resultatAddition = addition(5.5, 3.7);
+        System.out.println("Résultat de l'addition : " + resultatAddition);
+
+    }
+
+    // Méthode pour effectuer une addition
+    public static double addition(double a, double b) {
+        return a + b;
+    }
+
+   
+}
